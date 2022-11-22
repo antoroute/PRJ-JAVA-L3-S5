976ba5a44fdad4e73fdb7e16c7aa84fbfa029b5a
Astrostellar <laubertie.clement@gmail.com>
2022-11-22 18:23:27 +0100

    On a modifé le main

--- batiments/main.java
+++ batiments/main.java
@@ -2,6 +2,8 @@ public class main {
     public static void main(String[] args) {
         Maison m = new Maison("95 rue de la république", 16.8,5,18);
         System.out.println(m);
+        Maison m2 = new Maison("95 rue de la république", 16.8,5,18);
+        System.out.println(m2);
     }
 
     }
