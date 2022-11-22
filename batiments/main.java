433eb4f3bf838575c32db3c28c99ce37dc93d854
KycieK <victor.leonard41@gmail.com>
2022-11-22 16:44:49 +0100

    on a modif le main

--- batiments/main.java
+++ batiments/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        Maison m = new Maison(5,18, "95 rue de la république", 16.8);
+        Maison m = new Maison("95 rue de la république", 16.8,5,18);
         System.out.println(m);
     }
 
